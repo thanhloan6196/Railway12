9b25915558d4e732d45b42577bd3d255b8c4cc65
thanhloan6196 <daoloan6196@gmail.com>
2021-05-16 11:18:40 +0700

    Submit Assignment 2

---
+++ SQL/Assignment_1/Testing_System_Assignment_2.sql
@@ -0,0 +1,107 @@
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+CREATE DATABASE Testing_System_Assignment_1;
+
+USE Testing_System_Assignment_1;
+
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department (
+	DepartmentID		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName		NVARCHAR(50) NOT NULL UNIQUE KEY
+);
+
+DROP TABLE IF EXISTS `Position`;
+CREATE TABLE `Position` (
+	PositionID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	PositionName 		ENUM('Dev', 'Test', 'Scrum Master', 'PM') NOT NULL UNIQUE KEY
+);
+
+DROP TABLE IF EXISTS `Account`;
+CREATE TABLE `Account` (
+    AccountID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Email 				VARCHAR(100) NOT NULL UNIQUE KEY,
+    Username 			NVARCHAR(50) NOT NULL UNIQUE KEY,
+    FullName 			NVARCHAR(50) NOT NULL,
+    DepartmentID 		MEDIUMINT UNSIGNED NOT NULL,
+    PositionID 			MEDIUMINT UNSIGNED NOT NULL,
+    CreateDate 			DATETIME DEFAULT NOW(),
+    FOREIGN KEY (DepartmentID)  REFERENCES Department(DepartmentID),
+    FOREIGN KEY (PositionID) REFERENCES `Position`(PositionID)
+);
+
+DROP TABLE IF EXISTS `Group`;
+CREATE TABLE `Group` (
+    GroupID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    GroupName 			NVARCHAR(50) NOT NULL,
+    CreatorID 			MEDIUMINT UNSIGNED NOT NULL,
+    CreateDate 			DATETIME DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS GroupAccount;
+CREATE TABLE GroupAccount(
+   GroupID 				MEDIUMINT UNSIGNED,
+   AccountID 			MEDIUMINT UNSIGNED,
+   JoinDate 			DATETIME,
+   PRIMARY KEY (GroupID, AccountID),
+   FOREIGN KEY (GroupID) REFERENCES `Group`(GroupID),
+   FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID)
+);
+
+DROP TABLE IF EXISTS TypeQuestion;
+CREATE TABLE TypeQuestion (
+    TypeID 				MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TypeName 			ENUM('Essay', 'Multiple-Choice') NOT NULL
+);
+
+DROP TABLE IF EXISTS CategoryQuestion;
+CREATE TABLE CategoryQuestion (
+    CategoryID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CategoryName 		NVARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS Question;
+CREATE TABLE Question (
+    QuestionID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content 			NVARCHAR(100),
+    CategoryID 			MEDIUMINT UNSIGNED NOT NULL,
+    TypeID 				MEDIUMINT UNSIGNED NOT NULL,
+    CreatorID 			MEDIUMINT UNSIGNED NOT NULL,
+    CreateDate 			DATETIME DEFAULT NOW(),
+    FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY (TypeID) REFERENCES  TypeQuestion(TypeID),
+    FOREIGN KEY (CreatorID) REFERENCES `Account`(AccountID)
+);
+
+DROP TABLE IF EXISTS Answer;
+CREATE TABLE Answer (
+    AnswerID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content 			NVARCHAR(100) NOT NULL ,
+    QuestionID 			MEDIUMINT UNSIGNED NOT NULL,
+    isCorrect 			BIT,
+    FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+);
+
+DROP TABLE IF EXISTS Exam;
+CREATE TABLE Exam (
+    ExamID 				MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `Code` 				CHAR(50) NOT NULL,
+    Title 				NVARCHAR(50) NOT NULL,
+    CategoryID 			MEDIUMINT UNSIGNED NOT NULL,
+    Duration 			TIME NOT NULL,
+    CreatorID 			MEDIUMINT UNSIGNED NOT NULL,
+    CreateDate 			DATETIME DEFAULT NOW(),
+    FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY (CreatorID ) REFERENCES `Account`(AccountID)
+);
+
+DROP TABLE IF EXISTS ExamQuestion;
+CREATE TABLE ExamQuestion(
+	QuestionID 			 MEDIUMINT UNSIGNED NOT NULL,
+	ExamID				 MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (QuestionID, ExamID),
+    FOREIGN KEY (ExamID) REFERENCES Exam(ExamID),
+    FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+);
+
+
+
+
